eff59954461eccc25db8317bc658f639a9cf6b64
shalkovk <kshalkovskiy@gmail.com>
2026-03-04 23:46:12 +0500

    fixed database setup and added session dependencies in another file

--- backend/app/auth-service/db/database.py
+++ backend/app/auth-service/db/database.py
@@ -1,21 +1,14 @@
 import uuid
-from typing import Annotated, AsyncGenerator
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.ext.declarative import declarative_base
+from typing import Annotated
+from sqlalchemy.orm import mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 from config import database_url
 
 engine = create_async_engine(url=database_url)
-async_sessionmaker = async_sessionmaker(
+async_session_maker = async_sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False)
 str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
 
-Base = declarative_base()
 
-
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_sessionmaker() as session:
-        try:
-            yield session
-        finally:
-            await session.close
+class Base(DeclarativeBase):
+    pass

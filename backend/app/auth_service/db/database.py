e238b372f2cdb88c2bb72c2ad977a8e95a6dce71
shalkovk <kshalkovskiy@gmail.com>
2026-03-09 23:51:55 +0500

    added users and roles models

--- backend/app/auth_service/db/database.py
+++ backend/app/auth_service/db/database.py
@@ -2,9 +2,9 @@ import uuid
 from typing import Annotated
 from sqlalchemy.orm import mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
-from config import database_url
+from backend.app.auth_service.config import settings
 
-engine = create_async_engine(url=database_url)
+engine = create_async_engine(url=settings.database_url)
 async_session_maker = async_sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False)
 str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
--- backend/app/auth_service/models/users.py
+++ backend/app/auth_service/models/users.py
@@ -1,1 +1,44 @@
+from datetime import datetime
+from typing import Optional
+from db.database import str_uniq
+
+from sqlalchemy import String, DateTime, Boolean, func, text, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from db.database import Base
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str_uniq] = mapped_column(index=True)
+    users: Mapped[list["User"]] = relationship(back_populates="role")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id}, name={self.name})"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True, index=True)
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    email: Mapped[str_uniq] = mapped_column(index=True)
+    password: Mapped[str]
+    role_id: Mapped[int] = mapped_column(ForeignKey(
+        "roles.id"), default=1, server_default=text("1"), index=True)
+    role: Mapped["Role"] = relationship(
+        "Role", back_populates="users", lazy="joined")
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), onupdate=func.now(), nullable=True)
+    last_login_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), nullable=True)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id})"

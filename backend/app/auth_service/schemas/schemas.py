faf71d56ee45252363735214514f95d4399aa584
shalkovk <kshalkovskiy@gmail.com>
2026-03-10 23:32:58 +0500

    added pydantic schemas for user and role

--- backend/app/auth_service/schemas/schemas.py
+++ backend/app/auth_service/schemas/schemas.py
@@ -1,6 +1,7 @@
 import re
 from typing import Self
 from pydantic import BaseModel, ConfigDict, EmailStr, Field, field_validator, model_validator, computed_field
+from utils.utils import get_hashed_password
 
 
 class EmailModel(BaseModel):
@@ -25,5 +26,34 @@ class SUserRegister(UserBase):
     def check_password(self) -> Self:
         if self.password != self.confirm_password:
             raise ValueError("Password not matches")
-        self.password = get_password_hash(self.password)
+        self.password = get_hashed_password(self.password)
         return self
+
+
+class SUserAddDb(UserBase):
+    password: str = Field(min_length=8, max_length=50,
+                          description="Password in HASH-string format")
+
+
+class SUserAuth(EmailModel):
+    password: str = Field(min_length=8, max_length=50,
+                          description="Password, from 8 to 50 symbols")
+
+
+class RoleModel(BaseModel):
+    id: int = Field(description="User id")
+    name: str = Field(description="Role name")
+    model_config = ConfigDict(from_attributes=True)
+
+
+class SUserInfo(UserBase):
+    id: int = Field(description="User id")
+    role: RoleModel = Field(exclude=True)
+
+    @computed_field
+    def role_name(self) -> str:
+        return self.role.name
+
+    @computed_field
+    def role_id(self) -> int:
+        return self.role.id

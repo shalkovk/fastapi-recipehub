6e2e872ccef6ccb8a3b97a7eb6c0831d160133a9
shalkovk <kshalkovskiy@gmail.com>
2026-04-07 21:01:51 +0500

    user service class init

--- backend/app/auth_service/services/user_service.py
+++ backend/app/auth_service/services/user_service.py
@@ -1,7 +1,9 @@
 from db.database_deps import get_session_with_commit, get_session_without_commit
+from repositories.user_repository import UserRepository
 from config import settings
 from fastapi.requests import Request
 
 
 class UserService:
-    pass
+    def __init__(self, session):
+        self.repository = UserRepository(session)

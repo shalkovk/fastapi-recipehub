7e10cf92bd050e8d3af91baaa5b677762147d9e9
shalkovk <kshalkovskiy@gmail.com>
2026-04-08 23:18:15 +0500

    added register user async method

--- backend/app/auth_service/services/user_service.py
+++ backend/app/auth_service/services/user_service.py
@@ -1,9 +1,23 @@
 from db.database_deps import get_session_with_commit, get_session_without_commit
 from repositories.user_repository import UserRepository
+from schemas.schemas import SUserRegister, SUserInfo, SUserAddDb
+from sqlalchemy.ext.asyncio import AsyncSession
+from utils.exceptions import UserAlreadyExistsError
+from utils.utils import get_hashed_password
 from config import settings
 from fastapi.requests import Request
 
 
 class UserService:
-    def __init__(self, session):
-        self.repository = UserRepository(session)
+    def __init__(self):
+        self.repository = UserRepository
+
+    async def register_user(self, user_data: SUserRegister, session: AsyncSession) -> SUserInfo:
+        existing_user = await self.repository.get_user_by_email(user_data.email, session)
+        if existing_user:
+            raise UserAlreadyExistsError(
+                f"Username with email {user_data.email} already exists")
+
+        hashed_password = get_hashed_password(user_data.password)
+        user_to_save = SUserAddDb(email=user_data.email, first_name=user_data.first_name,
+                                  last_name=user_data.last_name, password=hashed_password)

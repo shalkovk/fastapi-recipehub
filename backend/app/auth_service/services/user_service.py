5564d108ed572401db6efe4c89da524b1a3591d2
shalkovk <kshalkovskiy@gmail.com>
2026-04-08 23:21:22 +0500

    added exceptions.py file

--- backend/app/auth_service/services/user_service.py
+++ backend/app/auth_service/services/user_service.py
@@ -2,7 +2,7 @@ from db.database_deps import get_session_with_commit, get_session_without_commit
 from repositories.user_repository import UserRepository
 from schemas.schemas import SUserRegister, SUserInfo, SUserAddDb
 from sqlalchemy.ext.asyncio import AsyncSession
-from utils.exceptions import UserAlreadyExistsError
+from utils.exceptions import UserAlreadyExistsException
 from utils.utils import get_hashed_password
 from config import settings
 from fastapi.requests import Request
@@ -15,7 +15,7 @@ class UserService:
     async def register_user(self, user_data: SUserRegister, session: AsyncSession) -> SUserInfo:
         existing_user = await self.repository.get_user_by_email(user_data.email, session)
         if existing_user:
-            raise UserAlreadyExistsError(
+            raise UserAlreadyExistsException(
                 f"Username with email {user_data.email} already exists")
 
         hashed_password = get_hashed_password(user_data.password)
---
+++ backend/app/auth_service/utils/exceptions.py
@@ -0,0 +1,4 @@
+from fastapi import status, HTTPException
+
+UserAlreadyExistsException = HTTPException(
+    status_code=status.HTTP_409_CONFLICT, detail="User already exists")

3a5b4ca819e68e757ca3eca8c9378b4203c4c49f
shalkovk <kshalkovskiy@gmail.com>
2026-04-12 22:49:44 +0500

    fix

--- backend/app/auth_service/services/user_service.py
+++ backend/app/auth_service/services/user_service.py
@@ -1,11 +1,12 @@
-from db.database_deps import get_session_with_commit, get_session_without_commit
+from datetime import datetime
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Response
+
 from repositories.user_repository import UserRepository
 from schemas.schemas import SUserRegister, SUserInfo, SUserAddDb, SUserAuth
-from sqlalchemy.ext.asyncio import AsyncSession
 from utils.exceptions import UserAlreadyExistsException, InvalidCredentialsException, UserNotFoundException
 from utils.utils import get_hashed_password, authenticate_user, set_tokens
-from config import settings
-from fastapi import Response
 
 
 class UserService:
@@ -36,3 +37,8 @@ class UserService:
         if not user:
             raise UserNotFoundException
         return SUserInfo.model_validate(user)
+
+    async def update_last_login(self, user_id: int, session: AsyncSession) -> None:
+        user = await self.repository.get_user_by_id(user_id, session)
+        if user:
+            user.last_login_at = datetime.utcnow()

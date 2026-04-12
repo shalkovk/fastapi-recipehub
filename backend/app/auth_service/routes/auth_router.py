178f2612cfb30430ae4b2ace12e335199ef7e262
shalkovk <kshalkovskiy@gmail.com>
2026-04-12 22:55:55 +0500

    fix

--- backend/app/auth_service/routes/auth_router.py
+++ backend/app/auth_service/routes/auth_router.py
@@ -15,9 +15,12 @@ router = APIRouter(
 @router.post("/register", response_model=SUserInfo, status_code=status.HTTP_201_CREATED)
 async def register_user(user_data: SUserRegister, session: AsyncSession = Depends(get_session_with_commit)):
     service = UserService()
-    return await service.register_user(user_data, session)
+    user_info = await service.register_user(user_data, session)
+    return user_info
 
 
 @router.post("/login", response_model=SUserInfo, status_code=status.HTTP_200_OK)
 async def login_user(auth_data: SUserAuth, response: Response, session: AsyncSession = Depends(get_session_with_commit)):
-    pass
+    service = UserService()
+    user_info = await service.login_user(auth_data, response, session)
+    return user_info

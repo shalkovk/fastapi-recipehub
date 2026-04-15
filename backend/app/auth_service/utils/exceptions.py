bf5b02c6a69f89f43f7cedfd6ab1b52550055830
shalkovk <kshalkovskiy@gmail.com>
2026-04-15 23:00:24 +0500

    added new exceptions

--- backend/app/auth_service/utils/exceptions.py
+++ backend/app/auth_service/utils/exceptions.py
@@ -1,4 +1,6 @@
 from fastapi import status, HTTPException
+# TokenNoFound, NoJwtException, TokenExpiredException, NoUserIdException, ForbiddenException, UserNotFoundException
+
 
 UserAlreadyExistsException = HTTPException(
     status_code=status.HTTP_409_CONFLICT, detail="User already exists")
@@ -8,3 +10,15 @@ InvalidCredentialsException = HTTPException(
 
 UserNotFoundException = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+TokenNotFound = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST, detail="Token not found in header")
+
+NoJwtException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+NoUserIdException = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND, detail="User id not found")
+
+ForbiddenException = HTTPException(
+    status_code=status.HTTP_403_FORBIDDEN, detail="Permission denied")
